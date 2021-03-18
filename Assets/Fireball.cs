b6dee0dbd5270731d57dfafc57df64948faf9780
Chuck <charles.karim00@gmail.com>
2021-03-18 17:00:59 -0400

    Trying to add a minimalist explosion effect

---
+++ Assets/Fireball.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    Vector3 minScale;
+    MeshRenderer mesh;
+    public Vector3 maxScale;
+    public bool repeatable;
+    public float speed = 2f;
+    public float duration = 5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody != null)
+        {
+            StartExplosion();
+        }
+    }
+
+    IEnumerator StartExplosion()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        minScale = mesh.transform.localScale;
+        while(repeatable)
+        {
+            yield return RepeatLerp(minScale, maxScale, duration);
+        }
+    }
+
+    public IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            mesh.transform.localScale = Vector3.Lerp(a, b, i);
+            Debug.Log(mesh.transform.localScale);
+            yield return null;
+        }
+    }
+}
